7754f4fe6b5fb19177079f49e5846f704510ed3a
bellaxscobie <bellaxscobie@gmail.com>
2024-08-28 07:57:46 +0000

    Project completegit add .

--- src/app.js
+++ src/app.js
@@ -2,10 +2,47 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  const number = generateRandomNumber();
+  const suit = generateRandomSuit();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardColor = suit === "♦" || suit === "♥" ? "red" : "black";
+
+  let top = document.querySelector(".top");
+  top.innerHTML = suit;
+  top.style.color = cardColor;
+
+  let setNumber = document.querySelector(".number");
+  setNumber.innerHTML = number;
+  setNumber.style.color = cardColor;
+
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = suit;
+  bottom.style.color = cardColor;
+};
+
+const generateRandomNumber = () => {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+const generateRandomSuit = () => {
+  const suits = ["♦", "♥", "♠", "♣"];
+  const indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
